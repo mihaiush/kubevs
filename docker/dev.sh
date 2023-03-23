29e3de6337008047b08dfde33ce211d67a58a23b
Mihai Craiu <mihai.craiu@1and1.ro>
2023-03-23 17:18:34 +0200

    fix

--- docker/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-cd /tmp
-
-https_proxy=http://popp-proxy-qa-bs01.po.server.lan:3128/ git clone https://github.com/mihaiush/kubevs.git
-cd kubevs
-git checkout dev
-
-/usr/bin/python3 $1
---
+++ docker/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+if grep -s __DEVMODE__ /etc/kubevs/config.yaml ; then
+    cd /tmp
+    https_proxy=http://popp-proxy-qa-bs01.po.server.lan:3128/ git clone https://github.com/mihaiush/kubevs.git
+    cd kubevs
+    git checkout dev
+    cd app
+else
+    cd /opt/kubevs
+fi
+
+exec /usr/bin/python3 $1
