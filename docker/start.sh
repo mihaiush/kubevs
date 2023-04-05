7cc39523c92ae30426204638ce114949feca3afc
Mihai Craiu <mihai.craiu@1and1.ro>
2023-04-05 12:47:00 +0300

    CURL_PROXY -> GIT_PROXY

--- docker/start.sh
+++ docker/start.sh
@@ -3,11 +3,11 @@
 if grep __DEVMODE__ /etc/kubevs/config.yaml ; then
     cd /tmp
     if [ -n "$PROXY" ] ; then
-        CURL_PROXY="https_proxy=${PROXY}"
+        GIT_PROXY="https_proxy=${PROXY}"
     else
-        CURL_PROXY=""
+        GIT_PROXY=""
     fi
-    $CURL_PROXY git clone https://github.com/mihaiush/kubevs.git
+    ${GIT_PROXY} git clone https://github.com/mihaiush/kubevs.git
     cd kubevs
     git checkout dev
     cd app
